895d8c13513b7f336f083662598d158a8e3b8a39
SiHoonChris <chrisleegpock@gmail.com>
2024-03-15 23:00:03 +0900

    Header - 2024.03.15

--- db/MySQL/Query/header/forGlobalIndexes.js
+++ db/MySQL/Query/header/forGlobalIndexes.js
@@ -1,18 +1,19 @@
 module.exports = {
     getGlobalIndexesData:
       { query:
-            'SELECT  DJI AS "Dow Jones" , \
-                     SPX AS "S&P 500" , \
-                     IXIC AS "Nasdaq" , \
-                     KOSPI , \
-                     BasisRate_KO AS "Basis Rate (KO)" , \
-                     BasisRate_US AS "Basis Rate (US)" , \
-                     USD_KRW AS "USD/KRW" , \
-                     CNY_KRW AS "CNY/KRW" , \
-                     HKD_KRW AS "HKD/KRW" , \
-                     SGD_KRW AS "SGD/KRW" \
-            FROM     global_index \
-            ORDER BY DATE DESC \
-            LIMIT    2'
+            'SELECT   g.DJI AS "Dow Jones", \
+                      g.SPX AS "S&P 500", \
+                      g.IXIC AS "Nasdaq", \
+                      g.KOSPI , \
+                      g.BasisRate_KO AS "Basis Rate (KO)" , \
+                      g.BasisRate_US AS "Basis Rate (US)" , \
+                      f.USD_KRW AS "USD/KRW" , \
+                      f.CNY_KRW AS "CNY/KRW" , \
+                      f.HKD_KRW AS "HKD/KRW" , \
+                      f.SGD_KRW AS "SGD/KRW" \
+             FROM     global_index g , fx_rate f \
+             WHERE    g.DATE = f.DATE \
+             ORDER BY g.DATE DESC \
+             LIMIT    2'
       }
 }
--- scripts/Header/GlobalIndexesAndFXRate.sql
+++ scripts/Header/GlobalIndexesAndFXRate.sql
@@ -1,4 +1,4 @@
--- 테이블 생성 --
+-- 테이블 생성 (지수, 금리 등) --
 CREATE TABLE global_index (
     DATE         DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
     DJI          DECIMAL(8, 2)  NOT NULL  DEFAULT 0, 
@@ -7,6 +7,12 @@ CREATE TABLE global_index (
     KOSPI        DECIMAL(8, 2)  NOT NULL  DEFAULT 0, 
     BasisRate_KO DECIMAL(4, 2)  NOT NULL  DEFAULT 0, 
     BasisRate_US DECIMAL(4, 2)  NOT NULL  DEFAULT 0, 
+	PRIMARY KEY(DATE)
+);
+
+-- 테이블 생성 (원 대비 환율) --
+CREATE TABLE fx_rate (
+    DATE         DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
     USD_KRW      DECIMAL(8, 2)  NOT NULL  DEFAULT 0, 
     CNY_KRW      DECIMAL(8, 2)  NOT NULL  DEFAULT 0, 
     HKD_KRW      DECIMAL(8, 2)  NOT NULL  DEFAULT 0, 
@@ -15,16 +21,17 @@ CREATE TABLE global_index (
 );
 
 -- 가장 최근자 지수 조회 --
-SELECT   DJI AS "Dow Jones", 
-         SPX AS "S&P 500", 
-         IXIC AS "Nasdaq", 
-         KOSPI , 
-         BasisRate_KO AS "Basis Rate (KO)" , 
-         BasisRate_US AS "Basis Rate (US)" , 
-         USD_KRW AS "USD/KRW" , 
-         CNY_KRW AS "CNY/KRW" ,  
-         HKD_KRW AS "HKD/KRW" ,  
-         SGD_KRW AS "SGD/KRW" 
-FROM     global_index 
-ORDER BY DATE DESC
+SELECT   g.DJI AS "Dow Jones", 
+         g.SPX AS "S&P 500", 
+         g.IXIC AS "Nasdaq", 
+         g.KOSPI , 
+         g.BasisRate_KO AS "Basis Rate (KO)" , 
+         g.BasisRate_US AS "Basis Rate (US)" , 
+         f.USD_KRW AS "USD/KRW" , 
+         f.CNY_KRW AS "CNY/KRW" ,  
+         f.HKD_KRW AS "HKD/KRW" ,  
+         f.SGD_KRW AS "SGD/KRW" 
+FROM     global_index g , fx_rate f
+WHERE    g.DATE = f.DATE
+ORDER BY g.DATE DESC
 LIMIT    2;
